807237b1020b7f92075d46eca4f8886dd7ad6370
tinfoil-knight <kunal99kundu@gmail.com>
2020-01-24 22:36:25 +0530

    Snake skeleton added

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 class Snake:
-    pass
+    def __init__(self, ini_body, ini_direction):
+        # ini is initial
+        self.body = ini_body
+        self.direction = ini_direction
+
+    def take_step(self, position):
+        self.body = self.body[1:] + [position]
+
+    def set_direction(self, direction):
+        self.direction = direction
+
+    def head(self):
+        return self.body[-1]
+
+
 class Apple:
     pass
+
+
 class Game:
+
     def __init__(self, height, width):
         self.height = height
         self.width = width
+
     def boardMatrix(self):
-        return [[" " for i in range(4)] for j in range(4)]
+        return [[" " for i in range(self.width)] for j in range(self.height)]
+
     def render(self):
         matrix = self.boardMatrix()
-        print("+----+")
+        print("+----------+")
         for row in matrix:
             print("|", end="")
             for cell in row:
                 print(cell, end="")
             print("|")
-        print("+----+")
-    pass
+        print("+----------+")
 
-game = Game(10, 20)
+game = Game(10, 10)
 game.render()
+# UP = (0, 1)
+# DOWN = (0, -1)
+# RIGHT = (1, 0)
+# LEFT = (-1, 0)
+# You can add for diagonal movements like L+U, R+U, L+D, R+D
+
+# Body is stored as an array of tuples(co-ordinates). That means when snake moves,
+# the coordinate of the last part is removed (first element of array)
+# New position is added to the end of the list(i.e. after snake's head)

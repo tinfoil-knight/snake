c4ecf2470767e66ae266541e8be1bf893fc95385
tinfoil-knight <kunal99kundu@gmail.com>
2020-01-25 11:15:06 +0530

    A little better but the code doesn't make sense now

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-UP = (0, 1)
-DOWN = (0, -1)
-RIGHT = (1, 0)
-LEFT = (-1, 0)
+RIGHT = (0, 1)
+LEFT = (0, -1)
+UP = (-1, 0)
+DOWN = (1, 0)
 
 class Snake:
     def __init__(self, ini_body, ini_direction):
